b68f6b389879d7d235c249eb3f6558856ce812aa
iamsuigeneris <irewolelanre@gmail.com>
2021-03-11 05:59:08 +0100

    part-four:Registration

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ const UserController = require('./controllers/UserController')
 const EventController = require('./controllers/EventController')
 const DashboardController = require('./controllers/DashboardController')
 const LoginController = require('./controllers/LoginController')
+const RegistrationController = require('./controllers/RegistrationController')
+const ApprovalController = require('./controllers/ApprovalController')
+const RejectionController = require('./controllers/RejectionController')
+
 const uploadConfig = require('./config/upload')
 
 const routes = express.Router()
@@ -14,6 +18,13 @@ routes.get('/status', (req,res) => {
     res.send({status: 200})
 })
 
+// Registration
+routes.post('/registration/:event_id',RegistrationController.create)
+routes.get('/registration/:registration_id',RegistrationController.getRegistration)
+routes.post('/registration/:registration_id/approvals',ApprovalController.approval)
+routes.post('/registration/:registration_id/rejections',RejectionController.rejection)
+
+ 
 // login
 routes.post('/login', LoginController.store)
 
